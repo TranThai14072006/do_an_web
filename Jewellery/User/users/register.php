20c30713bb7f17e62962791f50a9a7c6f5680e3d
jenny <phamquynhnhu07102006@gmail.com>
2026-03-20 23:36:52 +0700

    ...

--- Jewellery/User/users/register.php
+++ Jewellery/User/users/register.php
@@ -62,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="UTF-8">
   <title>Register</title>
 
-  <link rel="stylesheet" href="Register.css">
+  <link rel="stylesheet" href="../users/Register.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
