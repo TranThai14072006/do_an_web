0a19df09164297f5db04b14b7ac8c1369fd9a6ab
jenny <phamquynhnhu07102006@gmail.com>
2026-04-02 20:04:57 +0700

    fix link

--- Jewellery/User/indexprofile.php
+++ Jewellery/User/indexprofile.php
@@ -127,7 +127,7 @@ $link_profile = BASE_URL . 'User/users/profile.php';
 $link_logout  = BASE_URL . 'User/users/logout.php';
 $link_search  = BASE_URL . 'User/Search/search.html';
 $link_detail  = BASE_URL . 'User/users/product_detail.php';
-$link_shop    = BASE_URL . 'User/Search/search.html';
+$link_shop    = BASE_URL . 'User/Products/products.html';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -651,4 +651,4 @@ $link_shop    = BASE_URL . 'User/Search/search.html';
 </script>
 
 </body>
-</html>
+</html>
